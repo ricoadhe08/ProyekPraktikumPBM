9eb0c803670efbd3046bd8b55525ce3a6d6b6d16
ricoadhe08 <ricoadhe08@gmail.com>
2021-01-03 21:53:33 +0700

    modul 3
    modul 3

---
+++ HelloWorld/app/src/main/java/akakom/ricoadityaherlambang/helloworld/MainActivity.kt
@@ -0,0 +1,12 @@
+package akakom.ricoadityaherlambang.helloworld
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import akakom.ricoadityaherlambang.helloworld.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
